50d15c7162ad7ec6fd6b965ce5e8be5154b0e4cb
Oscar Alberto Tovar <33381041+oatovar@users.noreply.github.com>
2022-04-02 02:29:03 -0400

    chore: add landing page template (#1)

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'chore: deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:aiyaio/aiyaio.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
