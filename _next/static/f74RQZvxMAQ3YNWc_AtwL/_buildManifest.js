172e5cc6177b91509cb2c8520f69b63c8e61860f
Oscar Alberto Tovar <oscar.alberto.tovar@gmail.com>
2022-08-31 19:50:55 -0400

    chore: deploy

---
+++ _next/static/f74RQZvxMAQ3YNWc_AtwL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/815-a66d7535b225f3b3.js","static/chunks/pages/index-a7bf5dfd668d6db2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
